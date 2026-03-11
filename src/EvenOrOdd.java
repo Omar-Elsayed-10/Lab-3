57b621c18fdbbff59e6eb009923512afd2726bda
Omar-Elsayed-10 <265524896+Omar-Elsayed-10@users.noreply.github.com>
2026-03-11 11:23:17 +0100

    EvenOrOdd.java

--- src/EvenOrOdd.java
+++ src/EvenOrOdd.java
@@ -4,8 +4,6 @@ public class EvenOrOdd {
     int randomNumber = (int)(Math.random() * 100) + 1;
     System.out.println("Generated random number: " + randomNumber);
 
-    // 2. Check if the number is even or odd using modulo (%)
-    // The modulo operator returns the remainder of a division.
     if (randomNumber % 2 == 0) {
 
       System.out.println("The number " + randomNumber + " is EVEN because " + randomNumber + " % 2 = 0.");
