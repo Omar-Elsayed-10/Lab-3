168e53de98f8dc9fa113d72339cfbb0b409fce65
Omar-Elsayed-10 <265524896+Omar-Elsayed-10@users.noreply.github.com>
2026-03-11 11:21:53 +0100

    TaxCalculator.java

--- src/TaxCalculator.java
+++ src/TaxCalculator.java
@@ -3,12 +3,11 @@ import java.math.RoundingMode;
 
 public class TaxCalculator {
     public static void main(String[] args) {
-        // Constants
         double netPrice = 9.99;
         double vatRate = 0.23;
         int quantity = 10000;
 
-        // --- 1. CALCULATION USING DOUBLE ---
+       
         double grossPriceDouble = netPrice * (1 + vatRate);
         double totalGrossDouble = grossPriceDouble * quantity;
         double totalExcludingVatDouble = totalGrossDouble / (1 + vatRate);
@@ -18,8 +17,6 @@ public class TaxCalculator {
         System.out.println("Total Gross (10k pcs): " + totalGrossDouble);
         System.out.println("Total Net (Excluding VAT): " + totalExcludingVatDouble);
 
-        // --- 2. CALCULATION USING BIGDECIMAL ---
-        // Note: Always use the String constructor for BigDecimal to maintain precision!
         BigDecimal netBD = new BigDecimal("9.99");
         BigDecimal vatBD = new BigDecimal("0.23");
         BigDecimal quantityBD = new BigDecimal("10000");
@@ -27,7 +24,7 @@ public class TaxCalculator {
 
         BigDecimal grossPriceBD = netBD.multiply(one.add(vatBD));
         BigDecimal totalGrossBD = grossPriceBD.multiply(quantityBD);
-        // Dividing back to net requires a rounding mode
+
         BigDecimal totalExcludingVatBD = totalGrossBD.divide(one.add(vatBD), 2, RoundingMode.HALF_UP);
 
         System.out.println("\n--- Results using BIGDECIMAL ---");
